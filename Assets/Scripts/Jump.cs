f864cba8ac7e818dab63fa280da8b444bd1ba547
madeleoh <amoigus42069@proton.me>
2024-01-27 12:33:02 -0500

    mogus is sus

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rigidbody;
+    float jumpForce = 15;
+    public bool canJump;
+ 
+    void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (rigidbody.velocity.y > -.01 && rigidbody.velocity.y < .01)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        if (canJump && Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
